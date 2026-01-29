b9d097a6ab64387ca9e8c05ec3a228f00fd86bac
Benje1 <benjaminsal92@gmail.com>
2026-01-29 13:14:49 +0000

    Corrected table creating query

--- database/migration/001_init.sql
+++ database/migration/001_init.sql
@@ -24,21 +24,21 @@ CREATE TABLE shopping_items (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT NOT NULL,
     item_type shopping_item_type  NOT NULL
-)
+);
 
 CREATE TABLE shopping_list (
     shopping_item_id int REFERENCES shopping_items(id),
     quantity INT
-)
+);
 
 CREATE TABLE households (
     household_id INt GENERATED ALWAYS AS IDENTITY PRIMARY KEY
-)
+);
 
 CREATE TABLE users (
-    id PRIMARY KEY,
+    id GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     name TEXT,
     household INT REFERENCES households(household_id),
     username TEXT,
     password TEXT
-)
+);
