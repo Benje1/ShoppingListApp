ac4308897ba25ddc0bad6c85d75740e89e1c99fb
Benje1 <benjaminsal92@gmail.com>
2026-02-13 10:34:58 +0000

    Using sqlc instead of written queries

--- database/user_repo.go
+++ database/user_repo.go
@@ -3,6 +3,9 @@ package database
 import (
 	"context"
 
+	sqlc "weekly-shopping-app/database/sqlc"
+
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -11,13 +14,43 @@ type PostgresUserRepo struct {
 }
 
 func (p *PostgresUserRepo) InsertUser(ctx context.Context, name, username, passwordHash string, household uint) error {
-	return InsertUser(ctx, p.DB, name, username, passwordHash, household)
+	q := sqlc.New(p.DB)
+
+	return q.InsertUser(ctx, sqlc.InsertUserParams{
+		Name:         name,
+		Username:     username,
+		PasswordHash: passwordHash,
+		Household: pgtype.Int4{
+			Int32: int32(household),
+			Valid: true,
+		},
+	})
 }
 
 func (p *PostgresUserRepo) UpdateUser(ctx context.Context, username, name, passwordHash string) error {
-	return UpdateUser(ctx, p.DB, username, name, passwordHash)
+	q := sqlc.New(p.DB)
+
+	return q.UpdateUser(ctx, sqlc.UpdateUserParams{
+		Username:     username,
+		Name:         name,
+		PasswordHash: passwordHash,
+	})
 }
 
 func (p *PostgresUserRepo) GetUserByUsername(ctx context.Context, username string) (*User, error) {
-	return GetUserByUsername(ctx, p.DB, username)
+	q := sqlc.New(p.DB)
+
+	u, err := q.GetUserByUsername(ctx, username)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		Id:           int(u.ID),
+		Name:         u.Name,
+		Household:    int(u.Household.Int32),
+		Username:     u.Username,
+		PasswordHash: u.PasswordHash,
+		CreatedAt:    u.CreatedAt.Time.String(),
+	}, nil
 }
